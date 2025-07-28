d373a2d3dd3b9ac753b3954833e5e61a475a9fba
krunoslavrihtaric10-afk <krunoslav.rihtaric10@gmail.com>
2025-07-28 18:13:44 +0200

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,19 @@
+#a = input('Input some number: ')
+a = int(input("Enter some number: "))
+#b = input('Input second number: ')
+b = int(input("Enter second number: "))
+multi = a * b
+print ("Multiplication: ")
+print (multi)
+div = a / b
+print ("Division: ")
+print (div)
+mod = a % b
+print ("Modulo: ")
+print (mod)
+add = a + b
+print ("Addition: ")
+print (add)
+subtr = a - b
+print ("Subtraction: ")
+print (subtr)
