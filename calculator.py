ab4314990019faf64b5404d331275bf93f8568af
krunoslavrihtaric10-afk <krunoslav.rihtaric10@gmail.com>
2025-10-25 13:27:58 +0200

    Refactor calculator input and output handling
    Refactored calculator to improve input prompts and output formatting.

--- calculator.py
+++ calculator.py
@@ -1,19 +1,19 @@
-#a = input('Input some number: ')
-a = int(input("Enter some number: "))
-#b = input('Input second number: ')
-b = int(input("Enter second number: "))
-multi = a * b
-print ("Multiplication: ")
-print (multi)
-div = a / b
-print ("Division: ")
-print (div)
-mod = a % b
-print ("Modulo: ")
-print (mod)
-add = a + b
-print ("Addition: ")
-print (add)
-subtr = a - b
-print ("Subtraction: ")
-print (subtr)
+print("Input 1. number: ")
+# Takes the 1st number from keyboard 
+a = int(input())
+print("Input 2. number: ")
+# Takes the 2nd number from the keyboard
+b = int(input())
+c = a * b
+d = a / b
+e = a + b
+f = a - b
+# Prints results
+print("Result of multiplication: ")
+print(c)
+print("Result of division: ")
+print(d)
+print("Result of addition: ")
+print(e)
+print("Result of subtraction: ")
+print(f)
